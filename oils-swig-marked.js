32408d544ae95fe92e10f29954b1a08e2a7db7db
Manny <manuel_igop@yahoo.com>
2014-03-07 01:03:43 +0800

    cleaned logging

--- oils-swig-marked.js
+++ oils-swig-marked.js
@@ -3,11 +3,12 @@ module.exports = function(pkg, app) {
   var self = this;
 
   app.on('initializeServer', function() {
-    console.log('Initializing swig marked');
+    if (app.isDebug) {
+      console.log('Initializing swig marked');
+    }
     
     var swig = app.swig;
 
-    console.log('!'+swig);
     markedSwig.useFilter(swig);
     markedSwig.useTag(swig);
   })
